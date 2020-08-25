b42de65aeb377795f7dc374dc68e2fb17f2e37ed
Milod Kazerounian <milod.kazerounian@gmail.com>
2020-08-25 13:26:23 -0400

    add some more apps and journey DB

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,9 +13,19 @@ sudo -u postgres createuser vagrant --superuser
 sudo rm ~/mysql-apt-config_0.7.2-1_all.deb
 
 # Install RVM
-gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+echo 'HERE 1'
+#gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+#curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://rvm.io/mpapis.asc | gpg --batch --import -; \
+echo 409B6B1796C275462A1703113804BB82D39DC0E3:6: | gpg --batch --import-ownertrust; \
+# piotr.kuczynski@gmail.com
+curl -sSL https://rvm.io/pkuczynski.asc | gpg --batch --import -; \
+echo 7D2BAF1CF37B13E2069D6956105BD0E739499BDB:6: | gpg --batch --import-ownertrust
+echo 'HERE 2'
 curl -sSL https://get.rvm.io | bash -s stable
+echo 'HERE 3'
 source /home/vagrant/.rvm/scripts/rvm
+echo 'HERE 4'
 
 # Install Ruby
 rvm install 2.6.3
@@ -25,14 +35,56 @@ gem install bundler
 # Setup RDL
 echo "Installing RDL ..."
 git clone https://github.com/mckaz/dsl-infer-rdl
-cd rdl
+cd dsl-infer-rdl
 gem build rdl.gemspec
 gem install rdl-2.2.0.gem
 cd ~
 
-# Setup Discourse
+# Setup ActiveMerchant
 echo "Installing ActiveMerchant..."
 git clone https://github.com/mckaz/active_merchant_infer
 cd active_merchant_infer
 bundle install
+gem build activemerchant.gemspec
+gem install activemerchant-1.103.0.gem
+cd ~
+
+echo "Installing Diff-LCS..."
+git clone https://github.com/mckaz/diff-lcs-infer.git
+cd diff-lcs-infer
+bundle install
+gem build diff-lcs.gemspec
+gem install diff-lcs-1.4.gem
+cd ~
+
+echo "Installing Minimagick..."
+git clone https://github.com/mckaz/minimagick-infer.git
+cd minimagick-infer
+bundle install
+gem build mini_magick.gemspec
+gem install mini_magick-4.10.1.gem
+cd ~
+
+echo "Installing Optcarrot..."
+git clone https://github.com/mckaz/optcarrot-infer.git
+cd optcarrot-infer
+bundle install
+gem build optcarrot.gemspec
+gem install optcarrot-0.9.0.gem
+cd ~
+
+echo "Installing Sidekiq..."
+git clone https://github.com/mckaz/sidekiq-infer.git
+cd sidekiq-infer
+bundle install
+gem build sidekiq.gemspec
+gem install sidekiq-6.0.4.gem
+cd ~
+
+echo "Installing TZInfo..."
+git clone https://github.com/mckaz/tzinfo-infer.git
+cd tzinfo-infer
+bundle install
+gem build tzinfo.gemspec
+gem install tzinfo-2.0.0.gem
 cd ~
--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,4 @@
 export DEBIAN_FRONTEND=noninteractive
-
 debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-codename select trusty'
 debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-distro select ubuntu'
 debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-url string http://repo.mysql.com/apt/'
@@ -10,9 +9,10 @@ debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-tools selec
 debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/unsupported-platform select abort'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-
 wget http://dev.mysql.com/get/mysql-apt-config_0.7.2-1_all.deb
 dpkg -i mysql-apt-config_0.7.2-1_all.deb
 apt-get update
 apt-get install -y --force-yes mysql-server libmysqlclient-dev
+echo 'here1'
 mysql -u root -e "DROP USER 'root'@'localhost'; CREATE USER 'root'@'%' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+echo 'here2'
